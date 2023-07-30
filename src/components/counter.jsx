6e1ac921ae6e09b49edf4828522f5e4c6c774cce
Sumeda98 <ssbsankalpa@gmail.com>
2023-07-30 08:36:15 +0530

    update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,6 +17,7 @@ class Counter extends Component {
             >
               <i className="fa fa-plus-circle" aria-hidden="true" />
             </button>
+            <div></div>
             <button
               className="btn btn-info m-2"
               onClick={() => this.props.onDecrement(this.props.counter)}
